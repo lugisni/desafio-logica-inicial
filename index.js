2a5aa6a9ea136138fe45678df2db6197dc80023f
lugisni <ghosttlone67@gmail.com>
2023-12-10 18:37:45 -0300

    classificador de herói

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // VARIÁVEIS
-let name = ""
-let xp = ""
+let name = "lord"
+let xp = "3456"
 let level = ""
 
+// Estrutura de controle
+// Operadores
+
+if(xp <= 1000){
+    level = "Ferro"
+}else if (xp <= 2000){
+    level = "Bronze"  
+ }else if( xp <= 5000){
+    level = "Prata"
+ }else if(xp <= 6000){
+    level = "Ouro"
+ }else if(xp <= 7000){
+    level = "Platina"
+ }else if(xp <= 8000){
+    level = "Ascendente"
+ }else if(xp <= 9000){
+    level = "Imortal"
+ }else if(xp <= 10000){
+    level = "Radiante" }
+ console.log(" O Herói " + name + " Está na classe " + level)
